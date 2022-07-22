fd33044dbd304df1195229b3644e772c820bde47
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2022-07-22 16:53:03 +0800

    chore: use new ethereum-tx package (#1)
    * chore: use new ethereum-tx package
    
    * fix: index

--- dist/index.js
+++ dist/index.js
@@ -45,11 +45,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const events_1 = require("events");
 const ethUtil = __importStar(require("ethereumjs-util"));
-const ethereumjs_tx_1 = __importDefault(require("ethereumjs-tx"));
+const tx_1 = require("@ethereumjs/tx");
 const hdkey_1 = __importDefault(require("hdkey"));
 const trezor_connect_1 = __importDefault(require("trezor-connect"));
 const typedData_1 = __importDefault(require("trezor-connect/lib/plugins/ethereum/typedData"));
 const hdPathString = "m/44'/60'/0'/0";
+const SLIP0044TestnetPath = `m/44'/1'/0'/0`;
 const keyringType = 'Trezor Hardware';
 const pathBase = 'm';
 const MAX_INDEX = 1000;
@@ -58,8 +59,32 @@ const TREZOR_CONNECT_MANIFEST = {
     email: 'support@debank.com/',
     appUrl: 'https://debank.com/',
 };
+const ALLOWED_HD_PATHS = {
+    [hdPathString]: true,
+    [SLIP0044TestnetPath]: true,
+};
 function wait(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
+}
+/**
+ * @typedef {import('@ethereumjs/tx').TypedTransaction} TypedTransaction
+ * @typedef {InstanceType<import("ethereumjs-tx")>} OldEthJsTransaction
+ */
+/**
+ * Check if the given transaction is made with ethereumjs-tx or @ethereumjs/tx
+ *
+ * Transactions built with older versions of ethereumjs-tx have a
+ * getChainId method that newer versions do not.
+ * Older versions are mutable
+ * while newer versions default to being immutable.
+ * Expected shape and type
+ * of data for v, r and s differ (Buffer (old) vs BN (new)).
+ *
+ * @param {TypedTransaction | OldEthJsTransaction} tx
+ * @returns {tx is OldEthJsTransaction} Returns `true` if tx is an old-style ethereumjs-tx transaction.
+ */
+function isOldStyleEthereumjsTx(tx) {
+    return typeof tx.getChainId === 'function';
 }
 class TrezorKeyring extends events_1.EventEmitter {
     constructor(opts = {}) {
@@ -72,8 +97,29 @@ class TrezorKeyring extends events_1.EventEmitter {
         this.unlockedAccount = 0;
         this.paths = {};
         this.hdPath = '';
+        this.model = '';
         this.deserialize(opts);
-        trezor_connect_1.default.manifest(TREZOR_CONNECT_MANIFEST);
+        trezor_connect_1.default.on('DEVICE_EVENT', (event) => {
+            if (event && event.payload && event.payload.features) {
+                this.model = event.payload.features.model;
+            }
+        });
+        trezor_connect_1.default.init({ manifest: TREZOR_CONNECT_MANIFEST });
+    }
+    /**
+     * Gets the model, if known.
+     * This may be `undefined` if the model hasn't been loaded yet.
+     *
+     * @returns {"T" | "1" | undefined}
+     */
+    getModel() {
+        return this.model;
+    }
+    dispose() {
+        // This removes the Trezor Connect iframe from the DOM
+        // This method is not well documented, but the code it calls can be seen
+        // here: https://github.com/trezor/connect/blob/dec4a56af8a65a6059fb5f63fa3c6690d2c37e00/src/js/iframe/builder.js#L181
+        trezor_connect_1.default.dispose();
     }
     serialize() {
         return Promise.resolve({
@@ -89,7 +135,7 @@ class TrezorKeyring extends events_1.EventEmitter {
         this.hdPath = opts.hdPath || hdPathString;
         this.accounts = opts.accounts || [];
         this.page = opts.page || 0;
-        this.perPage = 5;
+        this.perPage = opts.perPage || 5;
         return Promise.resolve();
     }
     isUnlocked() {
@@ -211,52 +257,104 @@ class TrezorKeyring extends events_1.EventEmitter {
         }
         this.accounts = this.accounts.filter((a) => a.toLowerCase() !== address.toLowerCase());
     }
-    // tx is an instance of the ethereumjs-transaction class.
+    /**
+     * Signs a transaction using Trezor.
+     *
+     * Accepts either an ethereumjs-tx or @ethereumjs/tx transaction, and returns
+     * the same type.
+     *
+     * @template {TypedTransaction | OldEthJsTransaction} Transaction
+     * @param {string} address - Hex string address.
+     * @param {Transaction} tx - Instance of either new-style or old-style ethereumjs transaction.
+     * @returns {Promise<Transaction>} The signed transaction, an instance of either new-style or old-style
+     * ethereumjs transaction.
+     */
     signTransaction(address, tx) {
-        return new Promise((resolve, reject) => {
-            this.unlock()
-                .then((status) => {
-                setTimeout((_) => {
-                    trezor_connect_1.default.ethereumSignTransaction({
-                        path: this._pathFromAddress(address),
-                        transaction: {
-                            to: this._normalize(tx.to),
-                            value: this._normalize(tx.value),
-                            data: this._normalize(tx.data),
-                            chainId: tx._chainId,
-                            nonce: this._normalize(tx.nonce),
-                            gasLimit: this._normalize(tx.gasLimit),
-                            gasPrice: this._normalize(tx.gasPrice),
-                        },
-                    })
-                        .then((response) => {
-                        if (response.success) {
-                            tx.v = response.payload.v;
-                            tx.r = response.payload.r;
-                            tx.s = response.payload.s;
-                            const signedTx = new ethereumjs_tx_1.default(tx);
-                            const addressSignedWith = ethUtil.toChecksumAddress(`0x${signedTx.from.toString('hex')}`);
-                            const correctAddress = ethUtil.toChecksumAddress(address);
-                            if (addressSignedWith !== correctAddress) {
-                                reject(new Error('signature doesnt match the right address'));
-                            }
-                            resolve(signedTx);
-                        }
-                        else {
-                            reject(new Error((response.payload && response.payload.error) ||
-                                'Unknown error'));
-                        }
-                    })
-                        .catch((e) => {
-                        reject(new Error((e && e.toString()) || 'Unknown error'));
-                    });
-                    // This is necessary to avoid popup collision
-                    // between the unlock & sign trezor popups
-                }, status === 'just unlocked' ? DELAY_BETWEEN_POPUPS : 0);
-            })
-                .catch((e) => {
-                reject(new Error((e && e.toString()) || 'Unknown error'));
+        if (isOldStyleEthereumjsTx(tx)) {
+            // In this version of ethereumjs-tx we must add the chainId in hex format
+            // to the initial v value. The chainId must be included in the serialized
+            // transaction which is only communicated to ethereumjs-tx in this
+            // value. In newer versions the chainId is communicated via the 'Common'
+            // object.
+            return this._signTransaction(address, tx.getChainId(), tx, (payload) => {
+                tx.v = Buffer.from(payload.v, 'hex');
+                tx.r = Buffer.from(payload.r, 'hex');
+                tx.s = Buffer.from(payload.s, 'hex');
+                return tx;
             });
+        }
+        return this._signTransaction(address, tx.common.chainIdBN().toNumber(), tx, (payload) => {
+            // Because tx will be immutable, first get a plain javascript object that
+            // represents the transaction. Using txData here as it aligns with the
+            // nomenclature of ethereumjs/tx.
+            const txData = tx.toJSON();
+            // The fromTxData utility expects a type to support transactions with a type other than 0
+            txData.type = tx.type;
+            // The fromTxData utility expects v,r and s to be hex prefixed
+            txData.v = ethUtil.addHexPrefix(payload.v);
+            txData.r = ethUtil.addHexPrefix(payload.r);
+            txData.s = ethUtil.addHexPrefix(payload.s);
+            // Adopt the 'common' option from the original transaction and set the
+            // returned object to be frozen if the original is frozen.
+            return tx_1.TransactionFactory.fromTxData(txData, {
+                common: tx.common,
+                freeze: Object.isFrozen(tx),
+            });
+        });
+    }
+    /**
+     *
+     * @template {TypedTransaction | OldEthJsTransaction} Transaction
+     * @param {string} address - Hex string address.
+     * @param {number} chainId - Chain ID
+     * @param {Transaction} tx - Instance of either new-style or old-style ethereumjs transaction.
+     * @param {(import('trezor-connect').EthereumSignedTx) => Transaction} handleSigning - Converts signed transaction
+     * to the same new-style or old-style ethereumjs-tx.
+     * @returns {Promise<Transaction>} The signed transaction, an instance of either new-style or old-style
+     * ethereumjs transaction.
+     */
+    _signTransaction(address, chainId, tx, handleSigning) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let transaction;
+            if (isOldStyleEthereumjsTx(tx)) {
+                // legacy transaction from ethereumjs-tx package has no .toJSON() function,
+                // so we need to convert to hex-strings manually manually
+                transaction = {
+                    to: this._normalize(tx.to),
+                    value: this._normalize(tx.value),
+                    data: this._normalize(tx.data),
+                    chainId,
+                    nonce: this._normalize(tx.nonce),
+                    gasLimit: this._normalize(tx.gasLimit),
+                    gasPrice: this._normalize(tx.gasPrice),
+                };
+            }
+            else {
+                // new-style transaction from @ethereumjs/tx package
+                // we can just copy tx.toJSON() for everything except chainId, which must be a number
+                transaction = Object.assign(Object.assign({}, tx.toJSON()), { chainId, to: this._normalize(tx.to) });
+            }
+            try {
+                const status = yield this.unlock();
+                yield wait(status === 'just unlocked' ? DELAY_BETWEEN_POPUPS : 0);
+                const response = yield trezor_connect_1.default.ethereumSignTransaction({
+                    path: this._pathFromAddress(address),
+                    transaction,
+                });
+                if (response.success) {
+                    const newOrMutatedTx = handleSigning(response.payload);
+                    const addressSignedWith = ethUtil.toChecksumAddress(ethUtil.addHexPrefix(newOrMutatedTx.getSenderAddress().toString('hex')));
+                    const correctAddress = ethUtil.toChecksumAddress(address);
+                    if (addressSignedWith !== correctAddress) {
+                        throw new Error("signature doesn't match the right address");
+                    }
+                    return newOrMutatedTx;
+                }
+                throw new Error((response.payload && response.payload.error) || 'Unknown error');
+            }
+            catch (e) {
+                throw new Error((e && e.toString()) || 'Unknown error');
+            }
         });
     }
     signMessage(withAccount, data) {
@@ -288,7 +386,6 @@ class TrezorKeyring extends events_1.EventEmitter {
                         }
                     })
                         .catch((e) => {
-                        console.log('Error while trying to sign a message ', e);
                         reject(new Error((e && e.toString()) || 'Unknown error'));
                     });
                     // This is necessary to avoid popup collision
@@ -296,11 +393,13 @@ class TrezorKeyring extends events_1.EventEmitter {
                 }, status === 'just unlocked' ? DELAY_BETWEEN_POPUPS : 0);
             })
                 .catch((e) => {
-                console.log('Error while trying to sign a message ', e);
                 reject(new Error((e && e.toString()) || 'Unknown error'));
             });
         });
     }
+    /**
+     * EIP-712 Sign Typed Data
+     */
     signTypedData(address, data, { version }) {
         return __awaiter(this, void 0, void 0, function* () {
             yield wait(500);
@@ -348,6 +447,33 @@ class TrezorKeyring extends events_1.EventEmitter {
         this.unlockedAccount = 0;
         this.paths = {};
     }
+    /**
+     * Set the HD path to be used by the keyring. Only known supported HD paths are allowed.
+     *
+     * If the given HD path is already the current HD path, nothing happens. Otherwise the new HD
+     * path is set, and the wallet state is completely reset.
+     *
+     * @throws {Error} Throws if the HD path is not supported.
+     *
+     * @param {string} hdPath - The HD path to set.
+     */
+    // setHdPath(hdPath) {
+    //   if (!ALLOWED_HD_PATHS[hdPath]) {
+    //     throw new Error(
+    //       `The setHdPath method does not support setting HD Path to ${hdPath}`,
+    //     );
+    //   }
+    //   // Reset HDKey if the path changes
+    //   if (this.hdPath !== hdPath) {
+    //     this.hdk = new HDKey();
+    //     this.accounts = [];
+    //     this.page = 0;
+    //     this.perPage = 5;
+    //     this.unlockedAccount = 0;
+    //     this.paths = {};
+    //   }
+    //   this.hdPath = hdPath;
+    // }
     /* PRIVATE METHODS */
     _normalize(buf) {
         return ethUtil.bufferToHex(buf).toString();
