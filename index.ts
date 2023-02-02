44c9c66cdff7a9fb92f837a485789d1a95cf4bc1
heisenberg <heisenberg-2077@outlook.com>
2023-02-02 17:34:09 +0800

    fix: multiple device error

--- index.ts
+++ index.ts
@@ -568,11 +568,15 @@ class TrezorKeyring extends EventEmitter {
 
     for (let i = 0; i < addresses.length; i++) {
       const address = addresses[i];
-      const account = {
-        address,
-        index: this.indexFromAddress(address) + 1,
-      };
-      accounts.push(account);
+      try {
+        const account = {
+          address,
+          index: this.indexFromAddress(address) + 1,
+        };
+        accounts.push(account);
+      } catch (e) {
+        console.log('address not found', address);
+      }
     }
 
     return accounts;
