22daf4608d73646c759658ab96dcc0e3961b3d28
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-02-28 16:20:43 +0800

    chore: update trezor-connect and typescript

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -71,7 +75,7 @@ var LedgerHDPathType;
     LedgerHDPathType["LedgerLive"] = "LedgerLive";
     LedgerHDPathType["Legacy"] = "Legacy";
     LedgerHDPathType["BIP44"] = "BIP44";
-})(LedgerHDPathType = exports.LedgerHDPathType || (exports.LedgerHDPathType = {}));
+})(LedgerHDPathType || (exports.LedgerHDPathType = LedgerHDPathType = {}));
 function wait(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
